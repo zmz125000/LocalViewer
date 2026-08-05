75186836c843cbf3aeec19cb31334423ee9243a6
Jingxuan Zhou <zlx220000@msn.com>
2026-08-05 18:10:43 +0800

    bump to 1.9.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,9 +77,9 @@ android {
         applicationId = "moe.tarsin.localviewer"
         versionCode = 38
         versionName = if (snapshot) {
-            "1.8.9-SNAPSHOT"
+            "1.9.0-SNAPSHOT"
         } else {
-            "1.8.9"
+            "1.9.0"
         }
         buildConfigField("boolean", "SNAPSHOT", "$snapshot")
         buildConfigField("String", "RAW_VERSION_NAME", "\"$versionName\"")
