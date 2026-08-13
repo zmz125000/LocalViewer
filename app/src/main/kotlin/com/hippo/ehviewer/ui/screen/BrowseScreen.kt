1e17d6bb061cce0bbda591072d7ba9a2443be87b
Jingxuan Zhou <zlx220000@msn.com>
2026-08-13 13:39:32 +0800

    fixed build error

--- app/src/main/kotlin/com/hippo/ehviewer/ui/screen/BrowseScreen.kt
+++ app/src/main/kotlin/com/hippo/ehviewer/ui/screen/BrowseScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
