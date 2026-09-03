d2b9690f7971514e7a3fdc7021f9405e22ce1546
Jingxuan Zhou <zlx220000@msn.com>
2026-09-03 12:00:20 +0800

    bump to 1.11.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,9 +77,9 @@ android {
         applicationId = "moe.tarsin.localviewer"
         versionCode = 38
         versionName = if (snapshot) {
-            "1.11.3-SNAPSHOT"
+            "1.11.4-SNAPSHOT"
         } else {
-            "1.11.3"
+            "1.11.4"
         }
         buildConfigField("boolean", "SNAPSHOT", "$snapshot")
         buildConfigField("String", "RAW_VERSION_NAME", "\"$versionName\"")
