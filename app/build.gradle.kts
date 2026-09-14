e916ac90d43364fe73099ff9cb62c1d2a6916a76
Jingxuan Zhou <zlx220000@msn.com>
2026-09-14 23:17:12 +0800

    1.11.27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,9 +77,9 @@ android {
         applicationId = "moe.tarsin.localviewer"
         versionCode = 38
         versionName = if (snapshot) {
-            "1.11.26-SNAPSHOT"
+            "1.11.27-SNAPSHOT"
         } else {
-            "1.11.26"
+            "1.11.27"
         }
         buildConfigField("boolean", "SNAPSHOT", "$snapshot")
         buildConfigField("String", "RAW_VERSION_NAME", "\"$versionName\"")
